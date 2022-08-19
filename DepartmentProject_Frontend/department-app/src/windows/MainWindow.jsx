de6f47f4bf1e41399f4bc5780fb556651a491817
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-08-19 10:43:19 +0300

    Modifying directory name

---
+++ DepartmentProject_Frontend/department-app/src/windows/MainWindow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    color: red;
+`
+
+const MainWindow = () => {
+    return (
+        <Container>Main Window</Container>
+    )
+}
+
+export default MainWindow
